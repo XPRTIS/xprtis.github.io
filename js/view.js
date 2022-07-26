f674bd934d4694f4d1975be8d66ed30301973633
XPRTIS <106042354+XPRTIS@users.noreply.github.com>
2022-07-26 18:44:30 +0900

    Update view.js

--- js/view.js
+++ js/view.js
@@ -467,8 +467,8 @@ class StartScreenView extends View { // subclass of View
         });
         
         for (let i = 0; i < supportedLanguages.length; i++) {
-            let x = instructionsX - margin;
-            let y = instructionsY + instructionsHeight * i + margin * i;
+            let x = instructionsX - instructionsWidth - margin;
+            let y = instructionsY + instructionsHeight * i + 50 * i;
             let text = languageMap[supportedLanguages[i]];
             this.buttons.push(new Button(x, y, instructionsWidth, instructionsHeight, buttonColor, textColor, 
                 fontName, 20, text, true, () => {
