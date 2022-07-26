db72fbaee624a222cd666b9e30f8ef97d6f2c35d
XPRTIS <106042354+XPRTIS@users.noreply.github.com>
2022-07-26 14:03:11 +0900

    Update view.js

--- js/view.js
+++ js/view.js
@@ -586,7 +586,6 @@ class StartScreenView extends View { // subclass of View
             context.globalAlpha = 0.8;
             /// get width of text
             var width = 400;
-            context.fillStyle = 'rgba(0, 0, 0, 0.3)';
             context.roundRect((document.documentElement.clientWidth / 2) - width/2, 
             document.documentElement.clientHeight * 0.20, width, 230, 20).fill();
             // context.shadowColor = "#000";
