e28e98e076d688729af294ae079e5470742a403c
XPRTIS <106042354+XPRTIS@users.noreply.github.com>
2022-07-26 15:59:10 +0900

    Update MainCharacter.js

--- js/MainCharacter.js
+++ js/MainCharacter.js
@@ -5,7 +5,7 @@ class MainCharacter {
         this.speed = 20;
         this.bullets = 10;
         this.spriteImage = spriteImage;
-        this.gender = null;
+        this.gender = true;
         this.w = 2260 / (4 * 10);
         this.h = 740 / 10;
         this.frameIndex = 0;
