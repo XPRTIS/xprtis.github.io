1a299bb4448320754af48ae1f223af775432b6aa
XPRTIS <106042354+XPRTIS@users.noreply.github.com>
2022-07-28 14:02:22 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -134,7 +134,6 @@ function checkAllCollisions(bullets, hazards) {
         if (collisionCheckRect(mainCharacter, hazard)) {
             hazardRemoveList.push(hazard);
             mainCharacter.health -= hazard.healthLoss;
-            if (hazard.name != "Food") timeElapsed.hit = 0.6;
             if (mainCharacter.health > 100) mainCharacter.health = 100;
             hazard.playCollisionAudio();
         }
